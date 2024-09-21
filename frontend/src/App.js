096c27d64096556f60aaeff3c329f1e8eb8df804
Kess Entrata <kesselentrata@gmail.com>
2024-09-21 13:45:34 +0800

    setup pages and components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,32 +1,17 @@
-import logo from "./logo.svg";
 import "./App.css";
-import { DatePicker } from "antd";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HomePage, LoginPage, PostPage, SignupPage } from "./pages";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <div className="flex gap-10">
-          <img src={logo} className="w-20 h-20" alt="logo" />
-          <h1 className="text-purple-500 bg-blue-800">Hello world</h1>
-        </div>
-
-        <DatePicker />
-
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <h1 class="text-3xl font-bold underline">Hello world!</h1>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<LoginPage />} />
+        <Route path="/home" element={<HomePage />} />
+        <Route path="/post" element={<PostPage />} />
+        <Route path="/signup" element={<SignupPage />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ frontend/src/pages/index.js
@@ -0,0 +1,6 @@
+import HomePage from "./HomePage";
+import LoginPage from "./LoginPage";
+import PostPage from "./PostPage";
+import SignupPage from "./SignupPage";
+
+export { HomePage, LoginPage, PostPage, SignupPage };
